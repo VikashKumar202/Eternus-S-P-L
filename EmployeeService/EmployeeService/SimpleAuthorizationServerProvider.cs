0e80ae1fc2b42c9d5b0be218a7090fa4e1697109
Vikash Kumar <vikash.kumar@Eternus.local>
2019-08-30 18:43:28 +0530

    first token based authentication

---
+++ EmployeeService/EmployeeService/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,67 @@
+﻿using EmployeeService.Models;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Cors;
+
+
+namespace EmployeeService.Provider
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); //   
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (var db = new EmployeeDatabaseEntities1())
+            {
+                if (db != null)
+                {
+                    var empl = db.Employees;
+                    var user = db.Users;
+                    if (user != null)
+                    {
+                        if (!string.IsNullOrEmpty(user.Where(u => u.UserName == context.UserName && u.Password == context.Password).FirstOrDefault().Name))
+                        {
+
+
+                           
+                            identity.AddClaim(new Claim("Name", context.UserName));
+                            identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+
+                            context.Validated(identity);
+
+
+
+
+                            
+                        }
+                        else
+                        {
+                            context.SetError("invalid_grant", "Provided username and password is incorrect");
+                            context.Rejected();
+                        }
+                    }
+                }
+                else
+                {
+                    context.SetError("invalid_grant", "Provided username and password is incorrect");
+                    context.Rejected();
+                }
+                return;
+            }
+        }
+    }  
+}
